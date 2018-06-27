031251758c74669a5442125e3d716a7a2c80969a
marco.pieterse@gmail.com <mpieterse@iteonline.co.za>
2018-06-27 17:21:59 +0200

    git config change

---
+++ src/app/pages/home/country-dashboard/country-dashboard.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CountryModel} from '../../../models/country.model';
+import {StateModel} from '../../../models/states.model';
+
+@Component({
+  selector: 'app-country-dashboard',
+  templateUrl: './country-dashboard.component.html',
+  styleUrls: ['./country-dashboard.component.css']
+})
+export class CountryDashboardComponent implements OnInit {
+  @Input() activeCountry: CountryModel;
+  @Input() activeState: StateModel;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
